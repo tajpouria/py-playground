b3a642b10bbcb3b2d8ae0a837c10a67230bb76c7
tajpouria <tajpouria.dev@gmail.com>
2020-02-18 08:43:24 +0330

    Added -- home, about and layout template

--- flask-blog/flaskblog.py
+++ flask-blog/flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 app.config['SOME_KEY'] = os.getenv('SOME_KEY')
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About good</h1>"
+    return render_template('about.html', title="About")
