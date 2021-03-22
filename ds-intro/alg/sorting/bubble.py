1601a0d404fcc1ae175b5c9abb861f3cc9d3144a
geohot <george.hotz@machine.ventures>
2021-03-22 15:41:43 +0430

    Added -- BB sort

---
+++ ds-intro/alg/sorting/bubble.py
@@ -0,0 +1,11 @@
+def bubbleSort(arr):
+	for i in range(0, len(arr) - 1):
+		for j in range(0, len(arr) - 1 - i):
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+	return arr
+
+arr = [4,2,20,14,1,2,3]
+print(bubbleSort(arr))
+
