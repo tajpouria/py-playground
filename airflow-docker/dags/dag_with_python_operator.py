bd82e61665168cd92c434a8ccff488f29dd58c6e
tajpouria <tajpouria.dev@gmail.com>
2022-07-29 10:35:19 +0430

    feat: add python operator and xcom intro

--- airflow-docker/dags/dag_with_python_operator.py
+++ airflow-docker/dags/dag_with_python_operator.py
@@ -1,14 +1,28 @@
-from airflow import models
-from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from enum import Enum
+
+from airflow.models import DAG, TaskInstance
+from airflow.operators.python import PythonOperator
 
 
-def greet(name, age):
-    print(f"Hi my name is {name}, and I'm {age} years old!")
+class Task_IDs(Enum):
+    GREET = "GREET"
+    GET_NAME = "GET_NAME"
 
 
-with models.DAG(
-    dag_id="dag_with_python_operator_v2",
+def get_name(ti: TaskInstance) -> str:
+    ti.xcom_push(key="first_name", value="Jerry")
+    ti.xcom_push(key="last_name", value="Fridman")
+
+
+def greet(age: int, ti: TaskInstance) -> None:
+    first_name: str = ti.xcom_pull(task_ids=str(Task_IDs.GET_NAME), key="first_name")
+    last_name: str = ti.xcom_pull(task_ids=str(Task_IDs.GET_NAME), key="last_name")
+    print(f"Hi my name is {first_name} {last_name}, and I'm {age} years old!")
+
+
+with DAG(
+    dag_id="dag_with_python_operator_v6",
     start_date=datetime(2022, 7, 28, 12),
     schedule_interval="@daily",
     default_args={
@@ -18,7 +32,13 @@ with models.DAG(
     },
 ) as dag:
     greet_task = PythonOperator(
-        task_id="greet", python_callable=greet, op_kwargs=({"name": "tom", "age": 20})
+        task_id=str(Task_IDs.GREET),
+        python_callable=greet,
+        op_kwargs=({"age": 20}),
     )
 
-    greet_task
+    get_name_task = PythonOperator(
+        task_id=str(Task_IDs.GET_NAME), python_callable=get_name
+    )
+
+    get_name_task >> greet_task
